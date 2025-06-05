32cb19de99bac8e975fdae02287e415edbf9046b
Carolyn Cole <cac9@princeton.edu>
2025-06-05 13:43:48 -0400

    Adding forgotten config file for home page

--- hanami_bookshelf/config/routes.rb
+++ hanami_bookshelf/config/routes.rb
@@ -2,6 +2,6 @@
 
 module HanamiBookshelf
   class Routes < Hanami::Routes
-    # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
+    root to: "home.index"
   end
 end
