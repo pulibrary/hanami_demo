42f0f0d53d2b49837efdfec8f3cdb3455eb42a4b
Carolyn Cole <cac9@princeton.edu>
2025-06-05 14:10:27 -0400

    Adding Books listing
    Co-authored-by: Kate Lynch <kelynch@users.noreply.github.com>

--- hanami_bookshelf/app/views/books/index.rb
+++ hanami_bookshelf/app/views/books/index.rb
@@ -4,6 +4,12 @@ module HanamiBookshelf
   module Views
     module Books
       class Index < HanamiBookshelf::View
+        expose :books do
+          [
+            {title: "Test Driven Development"},
+            {title: "Practical Object-Oriented Design in Ruby"}
+          ]
+        end
       end
     end
   end
