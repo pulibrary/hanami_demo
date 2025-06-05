e321e10b9b5d1c328d477bbdeed19938ce4d587c
Carolyn Cole <cac9@princeton.edu>
2025-06-05 13:45:03 -0400

    Adding spec for book listing (no functionality existsi yet)
    Co-authored-by: Kate Lynch <kelynch@users.noreply.github.com>

---
+++ hanami_bookshelf/spec/features/books/index_spec.rb
@@ -0,0 +1,11 @@
+# spec/features/books/index_spec.rb
+
+RSpec.feature "Books index" do
+    it "shows a list of books" do
+      visit "/books"
+  
+      expect(page).to have_selector "li", text: "Test Driven Development"
+      expect(page).to have_selector "li", text: "Practical Object-Oriented Design in Ruby"
+    end
+  end
+  
