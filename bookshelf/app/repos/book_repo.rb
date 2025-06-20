c49bc7bc00e03d519ef5e777764bad5be2f65d71
Carolyn Cole <cac9@princeton.edu>
2025-06-20 08:26:37 -0400

    Added show action and got the test to pass

--- bookshelf/app/repos/book_repo.rb
+++ bookshelf/app/repos/book_repo.rb
@@ -3,6 +3,10 @@
 module Bookshelf
   module Repos
     class BookRepo < Bookshelf::DB::Repo
+
+      def get(id)
+        books.by_pk(id).one!
+      end
     end
   end
 end
--- bookshelf/config/routes.rb
+++ bookshelf/config/routes.rb
@@ -3,6 +3,7 @@
 module Bookshelf
   class Routes < Hanami::Routes
     # Add your routes here. See https://guides.hanamirb.org/routing/overview/ for details.
-    get "/books", to: "books.index"
+    get "/books", to: "books.index",  as: :books
+    get "/books/:id", to: "books.show"
   end
 end
